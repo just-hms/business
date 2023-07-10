59ffc291f572f5c64b4e39cbb5cc525dba645801
just-hms <alessandroversari1@gmail.com>
2023-07-10 23:07:30 +0200

    add: render.py

--- pipeline/render/render.py
+++ pipeline/render/render.py
@@ -1,42 +1,62 @@
 import json
+from datetime import datetime
 
 def generate_journal(json_file):
     # Read the JSON data from file
     with open(json_file, 'r') as file:
         data = json.load(file)
 
+    # Group entries by category
+    grouped_entries = {}
+    for entry in data:
+        category = entry['category']
+        if category in grouped_entries:
+            grouped_entries[category].append(entry)
+        else:
+            grouped_entries[category] = [entry]
+
     # Create the HTML string
     html = '''
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Journal</title>
+        <title>IntelliNews</title>
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-        <style>
-            .prose {
-                max-width: 48rem;
-                margin-left: auto;
-                margin-right: auto;
-                padding: 1rem;
-            }
-        </style>
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
     </head>
-    <body>
-        <div class="prose">
+    <body class="my-32">
+        <div class="prose prose lg:prose-xl mx-auto px-3">
     '''
 
-    # Generate the journal entries
-    for entry in data:
+    # Generate the journal entries for each category
+    for category, entries in grouped_entries.items():
         html += '''
         <div>
-            <h2 class="text-xl font-bold">{}</h2>
-            <p class="text-sm text-gray-500">{}</p>
-            <img src="{}" alt="Image" class="my-4 rounded">
-            <p>{}</p>
-            <a href="{}" class="text-blue-500 hover:underline">Read More</a>
-            <hr class="my-8">
+            <h1 class="text-center text-2xl font-bold">{}</h1>
+            <hr class="my-4">
+        '''.format(category)
+
+        for entry in entries:
+            html += '''
+            <div>
+                <h2>{}</h2>
+                <p class="text-sm text-gray-500">{}</p>
+                <img src="{}" alt="Image" class="my-4 max-w-lg mx-auto rounded">
+                <p>{}</p>
+                <a href="{}" class="text-blue-500 hover:underline">Read More</a>
+                <hr class="my-8">
+            </div>
+            '''.format(
+                entry['title'],     
+                datetime.strptime(entry['date'], "%Y-%m-%dT%H:%M:%SZ").strftime("%B %d, %Y"),
+                entry['urlToImage'], 
+                entry['summary'], 
+                entry['url'],
+            )
+
+        html += '''
         </div>
-        '''.format(entry['article'], entry['date'], entry['urlToImage'], entry['summary'], entry['url'])
+        '''
 
     # Complete the HTML string
     html += '''
@@ -48,6 +68,6 @@ def generate_journal(json_file):
     return html
 
 # Example usage
-json_file = 'data.json'  # Replace with the path to your JSON file
+json_file = 'data/final-with-weight.json'  # Replace with the path to your JSON file
 html = generate_journal(json_file)
 print(html)
